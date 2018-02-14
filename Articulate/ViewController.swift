a68b517fa27b8e07ba58abc456dfc365f19084a0
SlickJohnson <liwa.johnson@gmail.com>
2018-02-13 16:58:58 -0800

    Implement simple url list view

--- Articulate/ViewController.swift
+++ Articulate/ViewController.swift
@@ -8,18 +8,48 @@
 
 import UIKit
 
+typealias URLList = [URL?]?
+
 class ViewController: UIViewController {
+  /// Displays urls to articles.
+  @IBOutlet weak var tableView: UITableView!
+  /// Urls
+  var urls: URLList
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    urls = [
+      URL(string: "https://developer.apple.com/documentation/safariservices/sfsafariviewcontroller"), // Test normal page.
+      URL(string: "https://developer.apple.com/documentation/webkit/wkwebview"),
+      URL(string: "https://www.youtube.com/watch?v=mJl68R5z1vI"), // Test page with video.
+    ]
+
+    tableView.dataSource = self
+    tableView.delegate = self
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+}
 
+extension ViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    guard let urls = urls else { return 0 }
+    return urls.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell()
+    if let urls = urls, let url = urls[indexPath.row] {
+      cell.textLabel?.text = url.absoluteString
+    }
+    return cell
+  }
+}
+
+extension ViewController: UITableViewDelegate {
 
 }
 
